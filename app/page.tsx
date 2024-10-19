569e81c6c794faed0cb647e79d6751b1861f9ebd
Adeleye Matthew <88755064+madegit@users.noreply.github.com>
2024-10-19 07:54:26 +0000

    doc

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://resend.com/docs"
+          href="https://resend.com/docs/send-with-nextjs"
           target="_blank"
           rel="noopener noreferrer"
         >
